a686cac535b8be4a0f231e9740581a4d500c5fdb
fand <fand.gmork@gmail.com>
2018-03-06 13:34:48 +0900

    Update tests for changes in remark

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -6,27 +6,27 @@ import externalLinks from "../"
 
 const base = file => readFileSync(join(__dirname, "fixtures", file), "utf-8")
 
-test("should not add target and rel", () => {
-  let {contents} = remark().use(externalLinks, {target: null, rel: null}).use(html).process(base("input.md"))
+test("should not add target and rel", async () => {
+  let {contents} = await remark().use(externalLinks, {target: null, rel: null}).use(html).process(base("input.md"))
   expect(contents).toBe(base("output.target_no.rel_no.html"))
 })
 
-test("should add target='_blank'", () => {
-  let {contents} = remark().use(externalLinks, {target: "_blank", rel: null}).use(html).process(base("input.md"))
+test("should add target='_blank'", async () => {
+  let {contents} = await remark().use(externalLinks, {target: "_blank", rel: null}).use(html).process(base("input.md"))
   expect(contents).toBe(base("output.target_blank.rel_no.html"))
 })
 
-test("should add rel='nofollow'", () => {
-  let {contents} = remark().use(externalLinks, {target: null, rel: "nofollow"}).use(html).process(base("input.md"))
+test("should add rel='nofollow'", async () => {
+  let {contents} = await remark().use(externalLinks, {target: null, rel: "nofollow"}).use(html).process(base("input.md"))
   expect(contents).toBe(base("output.target_no.rel_nofollow.html"))
 })
 
-test("should add target='_blank' and rel='nofollow'", () => {
-  let {contents} = remark().use(externalLinks, {target: "_blank", rel: "nofollow"}).use(html).process(base("input.md"))
+test("should add target='_blank' and rel='nofollow'", async () => {
+  let {contents} = await remark().use(externalLinks, {target: "_blank", rel: "nofollow"}).use(html).process(base("input.md"))
   expect(contents).toBe(base("output.target_blank.rel_nofollow.html"))
 })
 
-test("should add target='_blank' and rel='nofollow noopener noreferrer'", () => {
-  let {contents} = remark().use(externalLinks).use(html).process(base("input.md"))
+test("should add target='_blank' and rel='nofollow noopener noreferrer'", async () => {
+  let {contents} = await remark().use(externalLinks).use(html).process(base("input.md"))
   expect(contents).toBe(base("output.target_blank.rel_all.html"))
 })
